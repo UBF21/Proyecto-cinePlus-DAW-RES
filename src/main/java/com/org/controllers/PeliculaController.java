06b0e68f54f33442191628852fafccadd56ad591
UBF21 <flashgames104@gmail.com>
2022-11-08 20:05:28 -0500

    agregando metodo get por id ,post y delete en el controlador de peliculas

--- src/main/java/com/org/controllers/PeliculaController.java
+++ src/main/java/com/org/controllers/PeliculaController.java
@@ -3,10 +3,10 @@ package com.org.controllers;
 import com.org.models.Pelicula;
 import com.org.repositories.IPeliculaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PeliculaController {
@@ -15,8 +15,28 @@ public class PeliculaController {
     IPeliculaRepository repositoryPelicula;
 
     @GetMapping("/peliculas")
-    public List<Pelicula> Listadopeliculas() {
+    List<Pelicula> Listadopeliculas() {
 
         return repositoryPelicula.findAll();
+    }
+
+    @GetMapping("/peliculas/{id}")
+    Pelicula obtenerPelicula(@PathVariable String id) {
+        Optional<Pelicula> pelicula = repositoryPelicula.findById(id);
+        if (pelicula.isPresent()) {
+            return pelicula.orElse(new Pelicula());
+        }
+
+        return pelicula.get();
+    }
+
+    @PostMapping("/peliculas")
+    Pelicula guardarPelicula(@RequestBody Pelicula pelicula) {
+        return repositoryPelicula.save(pelicula);
+    }
+
+    @DeleteMapping("/peliculas/{id}")
+    void eliminarPelicula(@PathVariable String id) {
+        repositoryPelicula.deleteById(id);
     }
 }
